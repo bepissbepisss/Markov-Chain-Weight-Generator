31a04a8a50a99c827fdc676c39c9cbbcd010bf35
Matt L <c00l@duck.com>
2025-02-11 15:17:51 -0500

    Added basic outline of program

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Stats project
+<header>Stats Project</header>
+
+This project creates a csv file that represents the probabilites of the next word of a sequence n a *Markov Chain*.
+
--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+/*
+*  PLAN
+* First go through the text and identify unique words
+* Create columns and rows of the matrix with each unique word
+*
+* For a given word, go through the whole text and count the number of times each different word comes after it. Possibly in vector of a class with a name and count as members. Do the division (to find probabilities) and add these numbers to the matrix. Having the words in the rows/colums and vector in the same order (alphabetical) might make writing the numbers easier.
+* 
+* Do this for all words of the matrix.
+*/
+
 int main () {
 	return 0;
 }
